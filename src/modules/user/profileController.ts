85946db1418fced5c27aa55e6b009f71a8d6536f
SandyTorresTavares <sandyt.tavares@gmail.com>
2026-05-15 15:54:48 -0300

    fix: profile name return

--- src/modules/user/profileController.ts
+++ src/modules/user/profileController.ts
@@ -2,12 +2,14 @@ import { Request, Response } from "express";
 
 export const getProfile = (req: Request, res: Response) => {
     // Acessar dados do usuário que vieram do token (via middleware)
+    const name = (req as any).user.name;
     const userId = (req as any).user.userId;
     const email = (req as any).user.email;
 
     return res.status(200).json({
         message: "Perfil acessado com sucesso!",
         user: {
+            name: name,
             id: userId,
             email: email,
         }
